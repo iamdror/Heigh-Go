e1c7587c32e30fa38dd7dcc2ea9b389021258dcf
iamdror <iamdror@gmail.com>
2024-01-08 10:48:41 +0200

    big bark and big barks

--- puppy/puppy.go
+++ puppy/puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/GoesToEleven/dog"
+
 func Barc() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barc())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
